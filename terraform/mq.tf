9fa9ec89a535f8d9bfe4c41e5b38b1fb0fc8e5d1
Maria Castilla <m.castillag@uniandes.edu.co>
2025-10-17 08:40:57 -0500

    ds

--- terraform/mq.tf
+++ terraform/mq.tf
@@ -41,34 +41,4 @@ resource "aws_lb_listener" "rabbitmq_amqp" {
     target_group_arn = aws_lb_target_group.rabbitmq_amqp_tg.arn
   }
 }
-resource "aws_mq_broker" "rabbitmq" {
-  broker_name = "${local.app_name}-rabbitmq"
-
-  engine_type        = "RabbitMQ"
-  engine_version     = "3.13"
-  host_instance_type = "mq.t3.micro"
-  deployment_mode    = "SINGLE_INSTANCE"
-
-  publicly_accessible = false
-  subnet_ids          = [aws_subnet.private[0].id]
-  security_groups     = [aws_security_group.rabbitmq.id]
-
-  user {
-    username = var.rabbitmq_username
-    password = var.rabbitmq_password
-  }
-
-  logs {
-    general = true
-  }
-
-  auto_minor_version_upgrade = true
-  apply_immediately          = true
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.app_name}-rabbitmq"
-    }
-  )
-}
+// Using RabbitMQ on ECS with an internal NLB for AMQP; managed Amazon MQ broker removed
